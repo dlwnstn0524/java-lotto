056748b9fa80effac5921ae9e2b0f34db20f722e
Long9725 <develop9725@gmail.com>
2024-06-11 15:42:36 +0900

    test: methodSource

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -61,5 +64,30 @@ class LottoTest {
         Lotto lotto = new Lotto(numbers);
         WinningLotto winningLotto = new WinningLotto(winningNumbers, bonus);
         assertThat(lotto.match(winningLotto)).isEqualTo(lottoRank);
+    }
+
+    @DisplayName("로또 당첨 테스트")
+    @ParameterizedTest(name = "{displayName} - {3}")
+    @MethodSource("provideLottoTestData")
+    void matchMethodTest(
+            @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> numbers,
+            @ConvertWith(StringToLottoNumberSetConverter.class) final Set<LottoNumber> winningNumbers,
+            @ConvertWith(StringToLottoNumberConverter.class) final LottoNumber bonus,
+            LottoRank lottoRank
+    ) {
+        Lotto lotto = new Lotto(numbers);
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonus);
+        assertThat(lotto.match(winningLotto)).isEqualTo(lottoRank);
+    }
+
+    private static Stream<Arguments> provideLottoTestData() {
+        return Stream.of(
+                Arguments.of("1 2 3 4 5 6", "1 2 3 4 5 6", "7", LottoRank.FIRST),
+                Arguments.of("1 2 3 4 5 6", "1 2 3 4 5 12", "6", LottoRank.SECOND),
+                Arguments.of("1 2 3 4 5 6", "1 2 3 4 5 12", "13", LottoRank.THIRD),
+                Arguments.of("1 2 3 4 5 6", "1 2 3 4 11 12", "13", LottoRank.FOURTH),
+                Arguments.of("1 2 3 4 5 6", "1 2 3 10 11 12", "13", LottoRank.FIFTH),
+                Arguments.of("1 2 3 4 5 6", "1 2 9 10 11 12", "13", LottoRank.NONE)
+        );
     }
 }
