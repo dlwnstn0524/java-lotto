0653db3d8858cd9af16180805462c72bc13d1e7d
Long9725 <develop9725@gmail.com>
2024-06-11 17:02:32 +0900

    test: LottoRank.from 메소드 테스트

--- src/test/java/lotto/LottoRankTest.java
+++ src/test/java/lotto/LottoRankTest.java
@@ -3,9 +3,9 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,5 +23,26 @@ class LottoRankTest {
     })
     void price(LottoRank lottoRank, long expectedPrice) {
         assertThat(lottoRank.getPrice()).isEqualTo(expectedPrice);
+    }
+
+    @DisplayName("로또 등수 테스트")
+    @ParameterizedTest(name = "{displayName} - 맞춘 개수 {0}개, 보너스 맞춘 여부 {1}, 결과 {2}등")
+    @MethodSource("rank")
+    void from(int unionCount, boolean bonusMatched, LottoRank expected) {
+        LottoRank actual = LottoRank.from(unionCount, bonusMatched);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> rank(){
+        return Stream.of(
+                Arguments.of(6, false, LottoRank.FIRST),
+                Arguments.of(5, true, LottoRank.SECOND),
+                Arguments.of(5, false, LottoRank.THIRD),
+                Arguments.of(4, false, LottoRank.FOURTH),
+                Arguments.of(3, false, LottoRank.FIFTH),
+                Arguments.of(2, false, LottoRank.NONE),
+                Arguments.of(1, false, LottoRank.NONE),
+                Arguments.of(0, false, LottoRank.NONE)
+        );
     }
 }
